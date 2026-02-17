39d27470ae6b4b29d35256bbacae9fba96b80c63
kelsey hudson <khudson@tsss.org>
2026-02-16 17:52:00 -0800

    Support for muzi works R1 Neo device
    Support for R1 Neo hardware. New variant and baseboard class.
    * Known issues:
      - power management is not currently supported
      - power off via long button press is not implemented
    
    Add support for Epson Seiko RX8130CE I2C Real-time clock.

---
+++ variants/muziworks_r1_neo/target.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#define RADIOLIB_STATIC_ONLY 1
+#include <RadioLib.h>
+#include <helpers/radiolib/RadioLibWrappers.h>
+#include <R1NeoBoard.h>
+#include <helpers/radiolib/CustomSX1262Wrapper.h>
+#include <helpers/AutoDiscoverRTCClock.h>
+#include <helpers/sensors/EnvironmentSensorManager.h>
+
+extern R1NeoBoard board;
+extern WRAPPER_CLASS radio_driver;
+extern AutoDiscoverRTCClock rtc_clock;
+extern EnvironmentSensorManager sensors;
+extern DISPLAY_CLASS display;
+extern MomentaryButton user_btn;
+
+bool radio_init();
+uint32_t radio_get_rng_seed();
+void radio_set_params(float freq, float bw, uint8_t sf, uint8_t cr);
+void radio_set_tx_power(uint8_t dbm);
+mesh::LocalIdentity radio_new_identity();
