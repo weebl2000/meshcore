c3f2be7ec41d0f38f98c4ed0f40aa810b916a3af
Wessel Nieboer <wessel@weebl.me>
2026-03-05 12:24:56 +0100

    fix adc multiplier

--- variants/muziworks_r1_neo/R1NeoBoard.h
+++ variants/muziworks_r1_neo/R1NeoBoard.h
@@ -5,7 +5,9 @@
 #include <helpers/NRF52Board.h>
 
 #define  PIN_VBAT_READ    31
-#define  ADC_MULTIPLIER   (3 * 1.73 * 1.187 * 1000)
+// Voltage divider ratio 1.667 calibrated by simon-muzi (Meshtastic PR #8716)
+// Adapted for MeshCore's default 3.6V analog reference
+#define  ADC_MULTIPLIER   (3.6 * 1.667 * 1000)
 
 class R1NeoBoard : public NRF52BoardDCDC {
 protected:
