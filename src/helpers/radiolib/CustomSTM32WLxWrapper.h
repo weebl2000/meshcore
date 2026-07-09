4b02a86e9b4f13acfed55ac1fda5867e22dfd8ce
Wessel Nieboer <wessel@weebl.me>
2026-07-09 15:30:38 +0200

    Dynamic CAD sensitivity
    Probe every 4 seconds to measure actual channel activity. If channel
    seems busy we adjust CAD to be less sensitive. If channel seems empty
    maximum CAD sensitivity.

--- src/helpers/radiolib/CustomSTM32WLxWrapper.h
+++ src/helpers/radiolib/CustomSTM32WLxWrapper.h
@@ -31,6 +31,7 @@ public:
     return packetScoreInt(snr, sf, packet_len);
   }
   uint8_t getSpreadingFactor() const override { return ((CustomSTM32WLx *)_radio)->spreadingFactor; }
+  uint8_t getCADDetPeakBase() const override { return getSpreadingFactor() + 13; }  // Semtech DS.SX1261-2 recommended detPeak
 
   void doResetAGC() override { sx126xResetAGC((SX126x *)_radio); }
 };
